468ee934660e96e815b67fad9e3c22c945e4baea
shridy2510 <111216751+shridy2510@users.noreply.github.com>
2024-08-01 09:04:20 +0700

    ok

---
+++ backend springboot itam/src/main/java/com/project/displayDto/UserDisplayDto.java
@@ -0,0 +1,19 @@
+package com.project.displayDto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+public class UserDisplayDto {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String userName;
+    private String userId;
+    private Set<String> roles;
+
+
+}
